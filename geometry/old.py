f738f1262faaef776f199d4b69d08116fff0edbf
전유민 <ychun25@gmail.com>
2025-12-14 17:36:07 +0900

    Initial commit: geometry warping

--- geometry/old.py
+++ geometry/old.py
@@ -1,7 +1,7 @@
 # geometry/old.py
 
 from .offsets import apply_offset
-from .landmarks import *
+from . import landmarks as L
 
 def apply_old(pts, strength=1.0):
     """
@@ -11,31 +11,31 @@ def apply_old(pts, strength=1.0):
     s = strength
 
     # Brows
-    apply_offset(pts, LEFT_BROW,  dx=+2*s, dy=+6*s)
-    apply_offset(pts, RIGHT_BROW, dx=-2*s, dy=+6*s)
+    apply_offset(pts, L.LEFT_BROW,  dx=+2*s, dy=+6*s)
+    apply_offset(pts, L.RIGHT_BROW, dx=-2*s, dy=+6*s)
 
     # Eyes
-    apply_offset(pts, UPPER_LID_L, dy=+10*s)
-    apply_offset(pts, UPPER_LID_R, dy=+10*s)
-    apply_offset(pts, LOWER_LID_L, dy=-4*s)
-    apply_offset(pts, LOWER_LID_R, dy=-4*s)
+    apply_offset(pts, L.UPPER_LID_L, dy=+10*s)
+    apply_offset(pts, L.UPPER_LID_R, dy=+10*s)
+    apply_offset(pts, L.LOWER_LID_L, dy=-4*s)
+    apply_offset(pts, L.LOWER_LID_R, dy=-4*s)
 
-    apply_offset(pts, LEFT_OUTER_CANTHUS,  dx=-3*s, dy=+12*s)
-    apply_offset(pts, RIGHT_OUTER_CANTHUS, dx=+3*s, dy=+12*s)
+    apply_offset(pts, L.LEFT_OUTER_CANTHUS,  dx=-3*s, dy=+12*s)
+    apply_offset(pts, L.RIGHT_OUTER_CANTHUS, dx=+3*s, dy=+12*s)
 
     # Lips
-    apply_offset(pts, UPPER_LIP_CENTER, dy=+4*s)
-    apply_offset(pts, LOWER_LIP_CENTER, dy=+8*s)
+    apply_offset(pts, L.UPPER_LIP_CENTER, dy=+4*s)
+    apply_offset(pts, L.LOWER_LIP_CENTER, dy=+8*s)
 
-    apply_offset(pts, LEFT_MOUTH_CORNER,  dx=-6*s, dy=+14*s)
-    apply_offset(pts, RIGHT_MOUTH_CORNER, dx=+6*s, dy=+14*s)
+    apply_offset(pts, L.LEFT_MOUTH_CORNER,  dx=-6*s, dy=+14*s)
+    apply_offset(pts, L.RIGHT_MOUTH_CORNER, dx=+6*s, dy=+14*s)
 
-    apply_offset(pts, UPPER_LIP_ARC, dy=+3*s)
-    apply_offset(pts, LOWER_LIP_ARC, dy=+6*s)
+    apply_offset(pts, L.UPPER_LIP_ARC, dy=+3*s)
+    apply_offset(pts, L.LOWER_LIP_ARC, dy=+6*s)
 
     # Cheeks & jaw
-    apply_offset(pts, LOWER_CHEEK_L, dx=-6*s, dy=+14*s)
-    apply_offset(pts, LOWER_CHEEK_R, dx=+6*s, dy=+14*s)
-    apply_offset(pts, JAWLINE, dy=+12*s)
+    apply_offset(pts, L.LOWER_CHEEK_L, dx=-6*s, dy=+14*s)
+    apply_offset(pts, L.LOWER_CHEEK_R, dx=+6*s, dy=+14*s)
+    apply_offset(pts, L.JAWLINE, dy=+12*s)
 
     return pts
--- geometry/young.py
+++ geometry/young.py
@@ -1,7 +1,7 @@
 # geometry/young.py
 
 from .offsets import apply_offset
-from .landmarks import *
+from . import landmarks as L
 
 def apply_young(pts, strength=1.0):
     """
@@ -11,34 +11,34 @@ def apply_young(pts, strength=1.0):
     s = 0.6 * strength  # young is weaker than old
 
     # Brows
-    apply_offset(pts, LEFT_BROW,  dy=-6*s)
-    apply_offset(pts, RIGHT_BROW, dy=-6*s)
+    apply_offset(pts, L.LEFT_BROW,  dy=-6*s)
+    apply_offset(pts, L.RIGHT_BROW, dy=-6*s)
 
     # Eyes (bigger)
-    apply_offset(pts, UPPER_LID_L, dy=-6*s)
-    apply_offset(pts, UPPER_LID_R, dy=-6*s)
-    apply_offset(pts, LOWER_LID_L, dy=+4*s)
-    apply_offset(pts, LOWER_LID_R, dy=+4*s)
+    apply_offset(pts, L.UPPER_LID_L, dy=-6*s)
+    apply_offset(pts, L.UPPER_LID_R, dy=-6*s)
+    apply_offset(pts, L.LOWER_LID_L, dy=+4*s)
+    apply_offset(pts, L.LOWER_LID_R, dy=+4*s)
 
-    apply_offset(pts, LEFT_OUTER_CANTHUS,  dx=+2*s, dy=-8*s)
-    apply_offset(pts, RIGHT_OUTER_CANTHUS, dx=-2*s, dy=-8*s)
+    apply_offset(pts, L.LEFT_OUTER_CANTHUS,  dx=+2*s, dy=-8*s)
+    apply_offset(pts, L.RIGHT_OUTER_CANTHUS, dx=-2*s, dy=-8*s)
 
-    apply_offset(pts, LEFT_INNER_CANTHUS,  dx=-2*s)
-    apply_offset(pts, RIGHT_INNER_CANTHUS, dx=+2*s)
+    apply_offset(pts, L.LEFT_INNER_CANTHUS,  dx=-2*s)
+    apply_offset(pts, L.RIGHT_INNER_CANTHUS, dx=+2*s)
 
     # Lips (smile)
-    apply_offset(pts, LEFT_MOUTH_CORNER,  dx=+6*s, dy=-8*s)
-    apply_offset(pts, RIGHT_MOUTH_CORNER, dx=-6*s, dy=-8*s)
+    apply_offset(pts, L.LEFT_MOUTH_CORNER,  dx=+6*s, dy=-8*s)
+    apply_offset(pts, L.RIGHT_MOUTH_CORNER, dx=-6*s, dy=-8*s)
 
-    apply_offset(pts, UPPER_LIP_CENTER, dy=-3*s)
-    apply_offset(pts, LOWER_LIP_CENTER, dy=-2*s)
+    apply_offset(pts, L.UPPER_LIP_CENTER, dy=-3*s)
+    apply_offset(pts, L.LOWER_LIP_CENTER, dy=-2*s)
 
-    apply_offset(pts, UPPER_LIP_ARC, dy=-2*s)
-    apply_offset(pts, LOWER_LIP_ARC, dy=-1*s)
+    apply_offset(pts, L.UPPER_LIP_ARC, dy=-2*s)
+    apply_offset(pts, L.LOWER_LIP_ARC, dy=-1*s)
 
     # Cheeks & jaw
-    apply_offset(pts, LOWER_CHEEK_L, dx=+4*s, dy=-8*s)
-    apply_offset(pts, LOWER_CHEEK_R, dx=-4*s, dy=-8*s)
-    apply_offset(pts, JAWLINE, dy=-4*s)
+    apply_offset(pts, L.LOWER_CHEEK_L, dx=+4*s, dy=-8*s)
+    apply_offset(pts, L.LOWER_CHEEK_R, dx=-4*s, dy=-8*s)
+    apply_offset(pts, L.JAWLINE, dy=-4*s)
 
     return pts
